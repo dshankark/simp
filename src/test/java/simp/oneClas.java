73ee2534f9492928952fe2fe04e56193f5999be4
dshankark <35366175+dshankark@users.noreply.github.com>
2019-04-20 11:47:33 +0530

    new repo

---
+++ src/test/java/simp/oneClas.java
@@ -0,0 +1,14 @@
+package simp;
+
+import org.testng.annotations.Test;
+
+public class oneClas {
+	@Test
+	public void one() {
+		System.out.println("one classs is executed");
+	}
+
+	public void oneTwo() {
+		System.out.println("onetwo classs is executed");
+	}
+}
